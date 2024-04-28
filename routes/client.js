82812b12bd8c67dd875e697697ccc3fd317d566a
jainbo28 <jainbo28@gmail.com>
2024-04-28 23:03:01 +0200

    client

--- routes/client.js
+++ routes/client.js
@@ -1,11 +1,35 @@
+
 const router = require('express').Router();
 const { app } = require('apico/server.js');
+const mysql = require('mysql2');
 /** Declare routes for Router */
 
+// http://localhost:3000/client/add
+router.post('/add', function( req, res ){
+    let { firstName, lastName, age, gender, email } = req.body;
+    // Create a pool
+    const pool = mysql.createPool({
+        host: 'localhost',
+        user:'root',
+        password:'yeb123456',
+        database:'client_mngmt'
+    });
 
-router.post('/add', async( req, res )=>{
-    res.json({"message": "APi works"});
+    // insert into tasks using body object. Use callbacks to handle errors
+    pool.query('INSERT INTO client (firstName, lastName, age, gender, email) VALUES(?,?,?,?,?)'
+    , [firstName, lastName, age, gender, email], (err, result)=>{
+
+        // Close the pool
+        pool.end();
+
+
+        if( err ) res.json( err );
+
+        res.json( result );
+
+        
+    });
 });
 
-
+// Create a prefix for tasks routes
 app.use( '/client', router );
